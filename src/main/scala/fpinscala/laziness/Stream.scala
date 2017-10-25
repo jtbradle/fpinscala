79ee1a366cdae578265844e8c6b1bd8f5b78ec50
Jerome Bradley <jerome.bradley@gmail.com>
2017-10-25 17:36:17 -0400

    Did more Stream exercises

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,6 +1,7 @@
 package fpinscala.laziness
 
 import fpinscala.errorhandling._
+import Stream._
 
 sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
@@ -12,6 +13,34 @@ sealed trait Stream[+A] {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
   }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => empty
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Empty => empty
+    case Cons(_, t) if n > 0 => t().take(n - 1)
+    case _ if n == 0 => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
+  def takeWhileViaFoldRight(f: A => Boolean): Stream[A] =
+    foldRight(empty: Stream[A])((h, t) => if(f(h)) cons(h, t) else empty)
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
