45d46b2e3f65509612bba611d2e12c21f4b8220c
Jerome Bradley <jerome.bradley@gmail.com>
2017-11-09 19:36:35 -0500

    Did more work. On exercise 6.11 now

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -19,7 +19,7 @@ object RNG {
     }
   }
 
-  type Rand[+A] = RNG => (A, RNG)
+  type Rand[A] = RNG => (A, RNG)
 
   val int: Rand[Int] = _.nextInt
 
@@ -35,6 +35,13 @@ object RNG {
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
     (if (i < 0) -(i + 1) else i, r)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
   }
 
   def double(rng: RNG): (Double, RNG) = {
@@ -90,20 +97,35 @@ object RNG {
     }
   }
 
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(aa => map(rb)(bb => f(aa, bb)))
+
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
     fs.foldRight(unit(List[A]()))((randA, randList) => map2(randA, randList)(_ :: _))
   }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+  }
 }
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => State.unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    flatMap(a => sb.map(b => f(a, b)))
+  def flatMap[B](f: A => State[S, B]): State[S, B] = {
+    State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
+  }
 }
 
 sealed trait Input
@@ -114,5 +136,7 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+  def unit[A,S](a: A): State[S, A] =
+    State(s => (a, s))
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
